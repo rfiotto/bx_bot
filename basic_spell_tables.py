911d679b3006cd2bc48d22ea52cd6477799086eb
Rabi Fiotto <42848107+rfiotto@users.noreply.github.com>
2018-08-31 18:03:19 -0400

    Create basic_spell_tables.py

---
+++ basic_spell_tables.py
@@ -0,0 +1,44 @@
+from weighted_list import *
+
+SPELL_TABLE_MU1 = WeightedList({'Charm Person':         1,
+                                'Detect Magic':         1,
+                                'Floating Disc':        1,
+                                'Hold Portal':          1,
+                                'Light':                1,
+                                'Magic Missile':        1,
+                                'Protection from Evil': 1,
+                                'Read Languages':       1,
+                                'Read Magic':           1,
+                                'Shield':               1,
+                                'Sleep':                1,
+                                'Ventriloquism':        1})
+
+SPELL_TABLE_MU2 = WeightedList({'Continual Light':  1,
+                                'Detect Evil':      1,
+                                'Detect Invisible': 1,
+                                'ESP':              1,
+                                'Invisibility':     1,
+                                'Knock':            1,
+                                'Levitate':         1,
+                                'Locate Object':    1,
+                                'Mirror Image':     1,
+                                'Phantasmal Force': 1,
+                                'Web':              1,
+                                'Wizard Lock':      1})
+
+SPELL_TABLE_MU3 = WeightedList({'Dispel Magic': 1,
+                                'Fire Ball':    1,
+                                'Fly':          1})
+
+SPELL_TABLE_CL1 = WeightedList({'Cure Light Wounds':        1,
+                                'Detect Evil':              1,
+                                'Detect Magic':             1,
+                                'Light':                    1,
+                                'Protection from Evil':     1,
+                                'Purify Food and Water':    1,
+                                'Remove Fear':              1,
+                                'Resist Cold':              1})
+
+SPELL_TABLE_CL2 = WeightedList({'Bless':                1,
+                                'Hold Person':          1,
+                                "Silence 15' Radius":   1})
